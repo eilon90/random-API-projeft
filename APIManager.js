d9b9697d09abc3bef1443213097558c2e4288275
eilon90 <eilon90@gmail.com>
2020-11-17 00:07:45 +0200

    improve project

--- APIManager.js
+++ APIManager.js
@@ -1,18 +1,55 @@
-// This is the class that will manage all your APIs
-
 class APIManager {
     constructor() {
-        this.data = {};
-    }
+        this.data = {
+            user: {
+                firstName: '',
+                lastName: '',
+                pictue: '',
+                city: '',
+                state: ''
+           },
+           quote: '',
+           pokemon: {
+               image: '',
+               name: ''
+           },
+           meat: '',
+           friends: [
+               {firstName: '', lastName: ''},
+               {firstName: '', lastName: ''},
+               {firstName: '', lastName: ''},
+               {firstName: '', lastName: ''},
+               {firstName: '', lastName: ''},
+               {firstName: '', lastName: ''}
+            ]
 
-    getData = (url, id, func) => {
+        },
+        
+        this.finishIn4 = 0;
+    };
+
+    getUser = func => {
         $.ajax({
             method: 'GET',
-            url: url,
+            url: 'https://randomuser.me/api?results=7',
             dataType: 'json',
             success: data => {
-                this.data[id] = data;
-                func();
+                this.data.user.firstName = data.results[0].name.first;
+                this.data.user.lastName = data.results[0].name.last;
+                this.data.user.picture = data.results[0].picture.medium;
+                this.data.user.city = data.results[0].location.city;
+                this.data.user.state = data.results[0].location.state;
+
+                for (let i = 1; i < data.results.length; i++) {
+                    this.data.friends[i - 1].firstName = data.results[i].name.first;
+                    this.data.friends[i - 1].lastName = data.results[i].name.last;
+                }
+                
+                this.finishIn4++;
+                if (this.finishIn4 === 4) {
+                    func();
+                    this.finishIn4 = 0;
+                }
             },
             error: function(xhr, text, error) {
                 console.log(text);
@@ -20,6 +57,63 @@ class APIManager {
         });
     }
 
+    getQuote = func => {
+        $.ajax({
+            method: 'GET',
+            url: 'https://api.kanye.rest',
+            dataType: 'json',
+            success: data => {
+                this.data.quote = data;
+                this.finishIn4++;
+                if (this.finishIn4 === 4) {
+                    func();
+                    this.finishIn4 = 0;
+                }
+            },
+            error: function(xhr, text, error) {
+                console.log(text);
+            }
+        });
+    }
+
+    getPokemon = func => {
+        $.ajax({
+            method: 'GET',
+            url: `https://pokeapi.co/api/v2/pokemon/${Math.ceil(Math.random() * 149)}`,
+            dataType: 'json',
+            success: data => {
+                this.data.pokemon.image = data.sprites.front_default;
+                this.data.pokemon.name = data.name;
+                this.finishIn4++;
+                if (this.finishIn4 === 4) {
+                    func();
+                    this.finishIn4 = 0;
+                }
+            },
+            error: function(xhr, text, error) {
+                console.log(text);
+            }
+        });
+    }
+
+    getMeat = func => {
+        $.ajax({
+            method: 'GET',
+            url: 'https://baconipsum.com/api/?type=meat-and-filler&paras=1',
+            dataType: 'json',
+            success: data => {
+                this.data.meat = data;
+                this.finishIn4++;
+                if (this.finishIn4 === 4) {
+                    func();
+                    this.finishIn4 = 0;
+                }
+            },
+            error: function(xhr, text, error) {
+                console.log(text);
+            }
+        });
+    }
 }
 
-const manager = new APIManager;
+const apiManager = new APIManager;
--- Renderer.js
+++ Renderer.js
@@ -1,23 +1,21 @@
-
 class Renderer {
-    constructor(id, properties, htmlClass) {
+    constructor(id, htmlClass) {
         this.id = id;
-        this.properties = properties;
         this.htmlClass = htmlClass;
     }
 
-    render() {
+    render(data) {
         $(`.${this.htmlClass}`).empty();
         const source = $(`#${this.id}`).html();
         const template = Handlebars.compile(source);
-        const newHTML = template(this.properties);
-        $(`.${this.htmlClass}`).append(newHTML);
-    }
-
-    renderFriends() {
-        const source = $(`#${this.id}`).html();
-        const template = Handlebars.compile(source);
-        const newHTML = template(this.properties);
+        const newHTML = template(data);
         $(`.${this.htmlClass}`).append(newHTML);
     }
 }
+
+const userRender = new Renderer('user-template', 'user-container');
+const quoRender = new Renderer('quote-template', 'quote-container');
+const pokeRender = new Renderer('pokemon-template', 'pokemon-container');
+const meatRender = new Renderer('meat-template', 'meat-container');
+const friRender = new Renderer('friends-template', 'friends-container');
+
